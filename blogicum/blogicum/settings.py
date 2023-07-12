154d1cbfb095994e87837368f237d9a283df4015
ЛюбаПарахона <parakhonya1@yandex.ru>
2023-07-12 19:21:43 +0300

    dff

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -86,9 +86,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'media'
