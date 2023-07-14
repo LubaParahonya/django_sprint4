ce2d6f5ddcc49223a81d48b07020b5a0535e8fff
ЛюбаПарахона <parakhonya1@yandex.ru>
2023-07-14 19:49:56 +0300

    hhhhhhh

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 
 from .models import Category, Location, Post
 
 admin.site.register(Category)
 admin.site.register(Location)
+
 
 @admin.register(Post)
 class UserAdmin(admin.ModelAdmin):
